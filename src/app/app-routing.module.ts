dd92b7032f45c775f2178bf3d05e685c081e6df2
runguo <lirunguo0113@gmail.com>
2019-08-06 17:45:33 +0800

    [add] Ionic 4 some component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,45 @@
+import { TabsPage } from './tabs/tabs.page';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+    {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+            {
+                path: 'home',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: './home/home.module#HomePageModule'
+                    }
+                ]
+            },
+            {
+                path: 'me',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: './me/me.module#MePageModule'
+                    }
+                ]
+            },
+            {
+                path: '',
+                redirectTo: '/tabs/home',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+    }
 ];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    constructor(
+        protected pickerController: PickerController
+    ) {
+
+    }
+
+    protected async onClick() {
+        const picker = await this.pickerController.create({
+            animated: true,
+            buttons: [{
+                text: 'Save',
+                handler: () => console.log('Clicked Save!')
+            }, {
+                text: 'Log',
+                handler: (val) => {
+                    console.log('Clicked Log. Do not Dismiss.', val);
+                    return false;
+                }
+            }],
+            columns: [
+                {
+                    name: 'hours',
+                    prefix: 'total',
+                    suffix: 'hours',
+                    options: [
+                        {
+                            text: '1',
+                            value: '01'
+                        },
+                        {
+                            text: '2',
+                            value: '02'
+                        }
+                    ]
+                }
+            ],
+            cssClass: 'picker-hours',
+            mode: 'ios',
+        });
+        picker.present();
+    }
+
+    loadData(event) {
+        setTimeout(() => {
+          console.log('Done');
+          event.target.complete();
+          // App logic to determine if all data is loaded
+          // and disable the infinite scroll
+          /* if (data.length === 1000) {
+            event.target.disabled = true;
+          } */
+        }, 500);
+      }
 
 }
