064a8a10c211e17820c1c8b89dee341329c434db
runguo <lirunguo0113@gmail.com>
2019-08-07 16:19:33 +0800

    [add] Virtual scroll. [update] Package version.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,58 +8,29 @@ import { PickerController } from '@ionic/angular';
 })
 export class HomePage {
 
+    protected data = [];
+
     constructor(
         protected pickerController: PickerController
     ) {
-
-    }
-
-    protected async onClick() {
-        const picker = await this.pickerController.create({
-            animated: true,
-            buttons: [{
-                text: 'Save',
-                handler: () => console.log('Clicked Save!')
-            }, {
-                text: 'Log',
-                handler: (val) => {
-                    console.log('Clicked Log. Do not Dismiss.', val);
-                    return false;
-                }
-            }],
-            columns: [
-                {
-                    name: 'hours',
-                    prefix: 'total',
-                    suffix: 'hours',
-                    options: [
-                        {
-                            text: '1',
-                            value: '01'
-                        },
-                        {
-                            text: '2',
-                            value: '02'
-                        }
-                    ]
-                }
-            ],
-            cssClass: 'picker-hours',
-            mode: 'ios',
-        });
-        picker.present();
+        for (let index = 0; index < 20; index++) {
+            this.data.push('infinite scroll');
+        }
     }
 
     loadData(event) {
         setTimeout(() => {
-          console.log('Done');
-          event.target.complete();
-          // App logic to determine if all data is loaded
-          // and disable the infinite scroll
-          /* if (data.length === 1000) {
-            event.target.disabled = true;
-          } */
+            for (let index = 0; index < 20; index++) {
+                this.data.push('infinite scroll');
+            }
+            console.log('Done');
+            event.target.complete();
+            // App logic to determine if all data is loaded
+            // and disable the infinite scroll
+            if (this.data.length === 100) {
+                event.target.disabled = true;
+            }
         }, 500);
-      }
+    }
 
 }
--- src/app/me/me.page.ts
+++ src/app/me/me.page.ts
@@ -1,15 +1,99 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-me',
-  templateUrl: './me.page.html',
-  styleUrls: ['./me.page.scss'],
+    selector: 'app-me',
+    templateUrl: './me.page.html',
+    styleUrls: ['./me.page.scss'],
 })
 export class MePage implements OnInit {
 
-  constructor() { }
+    items: any[] = [];
 
-  ngOnInit() {
-  }
+    constructor(
+        protected pickerController: PickerController
+    ) {
+        for (let i = 0; i < 1000; i++) {
+            this.items.push({
+                name: i + ' - ' + images[rotateImg],
+                imgSrc: getImgSrc(),
+                avatarSrc: getImgSrc(),
+                imgHeight: Math.floor(Math.random() * 50 + 150),
+                content: lorem.substring(0, Math.random() * (lorem.length - 100) + 100)
+            });
+
+            rotateImg++;
+            if (rotateImg === images.length) {
+                rotateImg = 0;
+            }
+        }
+    }
+
+    ngOnInit() {
+    }
+
+    protected async onClick() {
+        const picker = await this.pickerController.create({
+            animated: true,
+            buttons: [{
+                text: 'Save',
+                handler: () => console.log('Clicked Save!')
+            }, {
+                text: 'Log',
+                handler: (val) => {
+                    console.log('Clicked Log. Do not Dismiss.', val);
+                    return false;
+                }
+            }],
+            columns: [
+                {
+                    name: 'hours',
+                    prefix: 'total',
+                    suffix: 'hours',
+                    options: [
+                        {
+                            text: '1',
+                            value: '01'
+                        },
+                        {
+                            text: '2',
+                            value: '02'
+                        }
+                    ]
+                }
+            ],
+            cssClass: 'picker-hours',
+            mode: 'ios',
+        });
+        picker.present();
+    }
 
 }
+
+// tslint:disable-next-line:max-line-length
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+const images = [
+  'bandit',
+  'batmobile',
+  'blues-brothers',
+  'bueller',
+  'delorean',
+  'eleanor',
+  'general-lee',
+  'ghostbusters',
+  'knight-rider',
+  'mirth-mobile'
+];
+
+function getImgSrc() {
+  const src = `https://dummyimage.com/600x400/${Math.round( Math.random() * 99999)}/fff.png`;
+  rotateImg++;
+  if (rotateImg === images.length) {
+    rotateImg = 0;
+  }
+  return src;
+}
+
+let rotateImg = 0;
+
